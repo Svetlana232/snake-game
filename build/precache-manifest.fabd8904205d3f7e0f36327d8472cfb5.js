746d2474275fe108300b22444e709bad59701811
Svetlana232 <70659156+Svetlana232@users.noreply.github.com>
2020-11-28 14:27:04 +0200

    Add files via upload

---
+++ build/precache-manifest.fabd8904205d3f7e0f36327d8472cfb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0411b043c07d408daff66e2549111f78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a3402ed2fe06c66e89e",
+    "url": "/static/css/main.b81aef37.chunk.css"
+  },
+  {
+    "revision": "a890f4b4f6e746eac32e",
+    "url": "/static/js/2.549a741a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.549a741a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a3402ed2fe06c66e89e",
+    "url": "/static/js/main.3ca56c02.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
