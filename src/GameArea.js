ee9385838ba7c9e5a85d8f303a1783977792e62b
Svetlana232 <70659156+Svetlana232@users.noreply.github.com>
2020-11-30 19:07:57 +0200

    Add files via upload

--- src/GameArea.js
+++ src/GameArea.js
@@ -1,10 +1,20 @@
 import React from "react";
 
+const style = {
+  width: "100%",
+  height: "100%",
+  position: "fixed",
+  zIndex: "100",
+  left: "0",
+  top: "0",
+  backgroundColor: "rgba(0, 0, 0, 0.5)"
+};
+
 
 
 const gamearea = props =>
   props.show ? (
-    <div onClick={props.clicked}>
+    <div style={style} onClick={props.clicked}>
       {props.children}{" "}
     </div>
   ) : null;
